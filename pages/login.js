a68d599c01dea3ba1ec5b055eab4ab535f214e48
Cxxshyy <djjames.catlyn@gmail.com>
2021-03-26 11:21:44 +0000

    added google auth

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 import Head from "next/head";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
 
 function Login() {
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch(alert);
+  };
   return (
     <Container>
       <Head>
@@ -10,7 +14,9 @@ function Login() {
       </Head>
       <LoginContainer>
         <Logo src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" />
-        <Button variant="outlined">Sign in with Google</Button>
+        <Button onClick={signIn} variant="outlined">
+          Sign in with Google
+        </Button>
       </LoginContainer>
     </Container>
   );
