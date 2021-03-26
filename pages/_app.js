c78faeeabe2bf3dc60a0ace4da2f45a60c3f63aa
Cxxshyy <djjames.catlyn@gmail.com>
2021-03-26 10:44:44 +0000

    added login page

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import Login from "./login";
+import Loading from "../components/Loading";
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
 
